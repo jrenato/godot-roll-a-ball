1f8ea162acd00caf83da3cab4ce8113f253f1b7d
karvalho <renato@karvalho.com.br>
2023-05-04 20:10:13 -0300

    Fixed pickup rotation

--- levels/pickup.gd
+++ levels/pickup.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
-var rotation_vector : Vector3 = Vector3(1, 1, 0)
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var mesh_instance_3d : MeshInstance3D = $MeshInstance3D
 
+var rotation_speed : float = 0.8
 
 func _ready() -> void:
 	pass
@@ -10,7 +10,9 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	mesh_instance_3d.rotation += rotation_vector * delta
+	mesh_instance_3d.rotate_object_local(Vector3(1, 0, 0), 1 * delta * rotation_speed)
+	mesh_instance_3d.rotate_object_local(Vector3(0, 1, 0), 2 * delta * rotation_speed)
+	mesh_instance_3d.rotate_object_local(Vector3(0, 0, 1), 3 * delta * rotation_speed)
 
 
 #func tween_rotate_loop():
