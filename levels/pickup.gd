14eb9c1b9ec711ea69a536acc7707ab421d4472f
karvalho <renato@karvalho.com.br>
2023-06-10 15:20:35 -0300

    Cleaned up code in pickup script

--- levels/pickup.gd
+++ levels/pickup.gd
@@ -5,10 +5,6 @@ extends Area3D
 var rotation_speed : float = 0.8
 
 
-func _ready() -> void:
-	pass
-
-
 func _process(delta: float) -> void:
 	mesh_instance_3d.rotate_object_local(Vector3(1, 0, 0), 1 * delta * rotation_speed)
 	mesh_instance_3d.rotate_object_local(Vector3(0, 1, 0), 2 * delta * rotation_speed)
